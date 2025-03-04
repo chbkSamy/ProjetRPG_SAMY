d6420e80ae13ea07d1bffd5b120f17bca1e4628c
NicoChlebo <nico.chlebo@gmail.com>
2025-03-04 17:40:35 +0100

    refactor

--- personnages.py
+++ personnages.py
@@ -18,7 +18,7 @@ class Personnage (Classe):
     def nom_personnage():
         nom_perso = input("Entrez le nom de votre personnage: ")
         while len(nom_perso) < 2 or len(nom_perso) > 15:
-            print("Le nom de votre personnage doit être compris entre 3 et 15 caractères")
+            print("Le nom de votre personnage doit être compris entre 2 et 15 caractères")
             nom_perso = input("Entrez le nom de votre personnage: ")
         return nom_perso
 
--- test_personnages.py
+++ test_personnages.py
@@ -8,17 +8,17 @@ class TestPersonnage(unittest.TestCase):
     def test_nom_personnage_valide(self):
         with mock.patch('builtins.input', return_value="TestNom"):
             personnage = Personnage(nom_perso="", classe_perso=Guerrier())
-            self.assertEqual(Personnage.nom_personnage(), "TestNom")
+            self.assertTrue(len(Personnage.nom_personnage())>=2 <=15)
 
     def test_nom_personnage_trop_court(self):
         with mock.patch('builtins.input', side_effect=["T", "TestNom"]):
             personnage = Personnage(nom_perso="", classe_perso=Guerrier())
-            self.assertEqual(Personnage.nom_personnage(), "TestNom")
+            self.assertTrue(len(Personnage.nom_personnage())>=2)
 
     def test_nom_personnage_trop_long(self):
-        with mock.patch('builtins.input', side_effect=["UnNomBeaucoupTropLong", "TestNom"]): 
+        with mock.patch('builtins.input', side_effect=["UnNomQuiEstBeaucoupTropLong", "TestNom"]): 
             personnage = Personnage(nom_perso="", classe_perso=Guerrier())
-            self.assertEqual(Personnage.nom_personnage(), "TestNom")
+            self.assertTrue(len(Personnage.nom_personnage())<=15)
 
 if __name__ == '__main__':
     unittest.main()
