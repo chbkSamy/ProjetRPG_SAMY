41ada32c14f42eb59bc410e6f7135d702cb44bec
Samy <chebreksamy@gmail.com>
2025-03-04 14:24:59 +0100

    [FIX]: Remove the fonction from the main

--- personnage.py
+++ personnage.py
@@ -9,28 +9,39 @@ class Personnage(Classes):
         self.classe = classe.nom
 
 
-# def nom_personnage():
-#     print("Le nom du personnage doit avoir entre 3 et 10 caractères.")
-#     nom = input("Entrez le nom du personnage : ")
-#     while len(nom) < 3 or len(nom) > 10:
-#         print("Le nom du personnage doit avoir entre 3 et 10 caractères.")
-#         nom = input("Entrez le nom du personnage : ")
-#     return nom
+    @classmethod
+    def initialiser_personnage(cls):
+        nom = input("Entrez le nom du personnage : ")
+        while len(nom) < 3 or len(nom) > 10:
+            print("Le nom du personnage doit avoir entre 3 et 10 caractères.")
+            nom = input("Entrez le nom du personnage : ")
+        classes_disponibles = [
+            Classes("Guerrier", 150, 150, 15, 5, 12, 6, 8, 5, 10, 4),
+            Classes("Mage", 90, 150, 4, 15, 5, 12, 7, 6, 5, 10),
+            Classes("Voleur", 110, 70, 10, 7, 8, 7, 15, 12, 7, 6)
+        ]
+        print("Choisissez une classe :")
+        for i, classe in enumerate(classes_disponibles):
+            print(f"{i + 1}. {classe.nom}")
+        choix = int(input("Entrez le numéro de la classe choisie : "))
+        selected_classe = classes_disponibles[choix - 1]
+        return cls(nom, selected_classe)
+
+
+    def recapitulatif_personnage(self):
+        print("Recapitulatif du personnage :")
+        print(f"Nom : {self.nom_personnage}")
+        print(f"Classe : {self.classe}")
+        print(f"PV : {self.pv}")
+        print(f"PM : {self.pm}")
+        print(f"Force : {self.force}")
+        print(f"Intelligence : {self.intelligence}")
+        print(f"Defense : {self.defense}")
+        print(f"Resistance magique : {self.resistance_magique}")
+        print(f"Agilité : {self.agilite}")
+        print(f"Chance : {self.chance}")
+        print(f"Endurance : {self.endurance}")
+        print(f"Esprit : {self.esprit}")
 
 
 
-
-# def recapitulatif_personnage(personnage):
-#     print("Recapitulatif du personnage :")
-#     print(f"Nom : {personnage.nom_personnage}")
-#     print(f"Classe : {personnage.classe}")
-#     print(f"PV : {personnage.pv}")
-#     print(f"PM : {personnage.pm}")
-#     print(f"Force : {personnage.force}")
-#     print(f"Intelligence : {personnage.intelligence}")
-#     print(f"Defense : {personnage.defense}")
-#     print(f"Resistance magique : {personnage.resistance_magique}")
-#     print(f"Agilité : {personnage.agilite}")
-#     print(f"Chance : {personnage.chance}")
-#     print(f"Endurance : {personnage.endurance}")
-#     print(f"Esprit : {personnage.esprit}")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+guerrier = Classes("Guerrier", 150, 150, 15, 5, 12, 6, 8, 5, 10, 4)
+# mage = Classes("Mage", 90, 150, 4, 15, 5, 12, 7, 6, 5, 10)
+# voleur = Classes("Voleur", 110, 70, 10, 7, 8, 7, 15, 12, 7, 6)
+# classes_disponibles = [guerrier, mage, voleur]
