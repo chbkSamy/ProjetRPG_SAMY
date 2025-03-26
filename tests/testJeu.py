4794c52bf6c3f85941eef83b88ff6b8d23e04eaf
Samy <chebreksamy@gmail.com>
2025-03-26 15:09:34 +0100

    [FIX]: fix bug test

--- tests/testJeu.py
+++ tests/testJeu.py
@@ -5,8 +5,7 @@ from plateau.jeu import Jeu
 class TestJeu(unittest.TestCase):
     def setUp(self):
         self.view = MagicMock()
-      
-        patcher = patch('builtins.input', return_value='Test')
+        patcher = patch('builtins.input', side_effect=["Test", "1"])
         self.mock_input = patcher.start()
         self.addCleanup(patcher.stop)
         self.jeu = Jeu(self.view)
